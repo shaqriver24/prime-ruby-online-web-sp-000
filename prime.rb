4ac01b2dd917b13642b7a1d059357f34b75d46e3
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-15 09:25:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(int)
   return false if int < 2
-  (2...int).each { |num| return false if int % num == 0 }
+  (2...int).each { |factor| return false if int % factor == 0 }
   true
 end
