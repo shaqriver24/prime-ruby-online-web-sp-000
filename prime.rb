842ce976ce515332e922424f072df45b24444778
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-15 09:25:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,7 @@
 require 'benchmark'
 # Add  code here!
-def prime?(num)
-  return false if num < 2
-  (2...num).each { |num1| return false if num % num1 == 0 }
+def prime?(int)
+  return false if int < 2
+  (2...int).each { |factor| return false if int % factor == 0 }
   true
-end
-
-def another_prime?(num)
-  if num < 2
-    return false
-  end
-
-  (2...num).each do |num1|
-    if num % num1 == 0
-      return false
-    end
-  end
-  return true
-end
-
-Benchmark.bm(10) do |x|
-  x.report('Prime: ') { prime?(97) }
-  x.report('Another prime:') { another_prime?(97) }
 end
